ff3afeeb7ea09ad0712fdb850e9883a67e061759
Erick Lumunge <lumungep12@gmail.com>
2021-10-19 18:27:29 +0300

    controllers

---
+++ Controllers/PostController.js
@@ -0,0 +1,86 @@
+import mongoose from "mongoose";
+import PostModel from "../Models/PostModel.js";
+import { ValidatePosts } from "../validation.js";
+
+// get all posts by all users
+export const getPosts = async (req, res) => {
+	try {
+		const posts = await PostModel.find();
+		res.status(200).json(posts);
+	} catch (error) {
+		res.json({ message: error });
+	}
+};
+
+//get single post, all users
+export const getPost = async (req, res) => {
+	try {
+		const post = await PostModel.findById(req.params.id);
+		res.status(200).json(post);
+	} catch (error) {
+		res.json({ message: error });
+	}
+};
+
+// add posts for logged in users
+export const createPost = async (req, res) => {
+	// validate data
+	const { error } = ValidatePosts(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const post = new Post({
+		title: req.body.title,
+		description: req.body.description,
+		image: req.body.image,
+		date: req.body.date,
+	});
+
+	try {
+		const SavedPost = await post.save();
+		res.status(200).json(SavedPost);
+	} catch (error) {
+		res.json({ message: error });
+	}
+};
+
+// delete post for logged in users
+export const deletePost = async (req, res) => {
+	try {
+		const DeletedPost = await Post.remove({ _id: req.params.id });
+		res.status(200).json(DeletedPost);
+	} catch (error) {
+		res.json({ message: error });
+	}
+};
+
+// update title for logged in users
+export const updateTitle = async (req, res) => {
+	try {
+		const UpdatedTitle = await Post.updateOne(
+			{ _id: req.params.id },
+			{ $set: { title: req.body.title } }
+		);
+		res.status(200).json(UpdatedTitle);
+	} catch (error) {
+		res.json({ message: error });
+	}
+};
+
+// update whole post for logged in users
+export const updatePost = async (req, res) => {
+	try {
+		const WholeUpdatedPost = await Post.findOneAndUpdate(
+			{ _id: req.params.id },
+			{
+				$set: {
+					title: req.body.title,
+					description: req.body.description,
+					image: req.body.image,
+				},
+			}
+		);
+		res.status(200).json(WholeUpdatedPost);
+	} catch (error) {
+		res.json({ message: error });
+	}
+};
